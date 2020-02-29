3855a7443b6cebd2c61c5ca3f8c5edb020e437d7
Gabriel Cioci <ciocigabi@gmail.com>
2020-02-29 23:56:01 +0200

    Updates

---
+++ precache-manifest.69d0d039bef4ae373f3c4bd6312a6bd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209b0c800ed4c78847288ca1a240f903",
+    "url": "/lightsout-game/index.html"
+  },
+  {
+    "revision": "4cd5d02c5aacca90ba71",
+    "url": "/lightsout-game/static/css/main.a30a6a74.chunk.css"
+  },
+  {
+    "revision": "4c7abb296f8fffce3073",
+    "url": "/lightsout-game/static/js/2.ec55858b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lightsout-game/static/js/2.ec55858b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd5d02c5aacca90ba71",
+    "url": "/lightsout-game/static/js/main.deb67e1a.chunk.js"
+  },
+  {
+    "revision": "c6a87eb84db4c41e5f31",
+    "url": "/lightsout-game/static/js/runtime-main.af9c2a1a.js"
+  },
+  {
+    "revision": "97e9809afe87374115926e4fbb5cfea6",
+    "url": "/lightsout-game/static/media/win.97e9809a.mp3"
+  }
+]);
